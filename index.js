3ffe75062b20a2bc2af72b17a1b8c15dec4257af
Максим Кирсанов <kirsmaxi@gmail.com>
2024-03-01 20:49:48 +0300

    trst

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ const staticFilesPath = path.join(__dirname, "dist");
 app.use(express.static(staticFilesPath));
 app.use(express.json());
 app.use(
-  cors({ origin: "https://guitars-store.onrender.com", credentials: true })
+  cors({ origin: "http://localhost:5173", credentials: true })
+  // cors({ origin: "https://guitars-store.onrender.com, credentials: true })
 );
 app.use(cookieParser());
 const storage = multer.memoryStorage();
